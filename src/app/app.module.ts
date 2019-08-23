7e37aa296ca602bdf762e663dc4d382bb10cfb9d
Bartosz <basakbartosz@gmail.com>
2019-08-23 09:14:02 +0200

    Wygenerowanie komponentu home.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AngularFontAwesomeModule,
     AppRoutingModule
   ],
   providers: [],
