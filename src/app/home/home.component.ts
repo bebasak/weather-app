c75f144c8fb35ca5b9838dfd4b1da45569948407
Bartosz <basakbartosz@gmail.com>
2019-08-26 12:53:54 +0200

    Dodanie prognozy pogody na najbliższe 5 dni.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,19 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { HomeService } from '../services/home.service';
 import { AppError } from '../common/app-error';
 import { NotFoundError } from '../common/not-found-error';
-import { slide } from '../animations';
+import { slide, fade } from '../animations';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   animations: [
-    slide
+    slide,
+    fade
   ]
 })
 export class HomeComponent implements OnInit {
   weatherData: any;
-
+  forecastData: any = [];
+  testData = [];
   constructor(private service: HomeService) { }
 
   ngOnInit() {
@@ -23,6 +25,36 @@ export class HomeComponent implements OnInit {
   del() {
     if(this.weatherData)
       this.weatherData = null;
+    if(this.forecastData)
+      this.forecastData = [];
+  }
+
+  getForecastWeather(input: HTMLInputElement) {
+    let city: any = input.value
+
+    this.service.getForecastWeather(city)
+      .subscribe(
+        response => {
+          console.log(response['list'].length);
+          for(let i = 0; i < response['list'].length; i++){
+            if (i%8 === 0){
+              console.log(response['list'][i])
+              this.forecastData.push(response['list'][i]);
+              console.log('dodano:');
+              console.log(this.forecastData);
+            }
+          }
+          //this.forecastData = response;
+        },
+        (error: AppError) => {
+          this.forecastData = [];
+          if (error instanceof NotFoundError) {
+            alert('Nie znaleziono.');
+          }
+          else {
+            throw error;
+          }
+        });
   }
 
   getData(input: HTMLInputElement) {
